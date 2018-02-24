2b89d3899ee3e0e6c0722f67f62830f099b7e653
Japjot Singh <japjot@airbears2-10-142-38-100.airbears2.1918.berkeley.edu>
2018-02-24 13:52:11 -0800

    Implements ASync Thread

--- app/src/main/java/com/example/japjot/pokedex/Utils.java
+++ app/src/main/java/com/example/japjot/pokedex/Utils.java
@@ -1,14 +1,10 @@
 package com.example.japjot.pokedex;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,69 +18,31 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 import static com.example.japjot.pokedex.Utils.BASEURL;
 import static com.example.japjot.pokedex.Utils.convertStreamToString;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by japjot on 2/24/18.
+ */
 
+public class Utils {
+    final static String BASEURL= "https://pokeapi.co/api/v2/pokemon/";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                (new FetchPokemonTest(query.toLowerCase())).execute();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        return true;
-    }
-
-    class FetchPokemonTest extends AsyncTask<Void, Void, JSONObject>{
-        String pokemonName;
-
-        FetchPokemonTest(String pokemonName){
-            this.pokemonName = pokemonName;
+    class FetchPokemonRunnable implements Runnable{
+        JSONObject jsonObject;
+        void setJSONObject(JSONObject jsonObject) {
+            this.jsonObject = jsonObject;
         }
 
         @Override
-        protected JSONObject doInBackground(Void... voids) {
-            try {
-                URL url = new URL(BASEURL+pokemonName);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                InputStream in = new BufferedInputStream(conn.getInputStream());
-                String response = convertStreamToString(in);
-                return new JSONObject(response);
-            } catch (JSONException | IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject jsonObject) {
+        public void run() {
             if(jsonObject != null){
                 TextView name, attack, defense, spatk, spdef, hp, speed;
                 ImageView imageView;
@@ -119,6 +77,40 @@ public class MainActivity extends AppCompatActivity {
             else{
                 Toast.makeText(MainActivity.this, "not a pokemon", Toast.LENGTH_SHORT).show();
             }
+        }
+        }
+    }
+
+    static class FetchPokemonTest extends AsyncTask<Void, Void, JSONObject> {
+        String args;
+        Runnable r;
+        Activity activity;
+
+        FetchPokemonTest(String pokemonName, MainActivity.FetchPokemonRunnable r, Activity activity){
+            this.args = pokemonName;
+            this.r = r;
+            this.activity = activity;
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... voids) {
+            try {
+                URL url = new URL(BASEURL+ args);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                String response = convertStreamToString(in);
+                return new JSONObject(response);
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            r.setJSONObject(jsonObject);
+            activity.runOnUiThread(r);
         }
     }
 }
